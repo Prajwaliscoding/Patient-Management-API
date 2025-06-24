505eb98c6d4ba1f89a312708d2f537cbbb56ccd6
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-06-24 14:15:19 -0500

    Added Query Parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -30,4 +30,19 @@ def patient_id(patient_id : str = Path(..., description = "ID of the patient.", 
         return data[patient_id]
     raise HTTPException(status_code = 404 , detail = "Patient Not Found.")
 
+@app.get("/sort_patient")
+def sort_patient(sort_by : str = Query(...,description ="Sort on the basis of height or weight"),
+                 order : str = Query("asc", description = "sort in asc or desc order")):
+    
+    data = patients_data()
 
+    if sort_by not in ["height", "weight"]:
+        raise HTTPException(status_code = 400 , detail = "Invalid field selection. Choose height or weight.")
+    if order not in ["asc", "desc"]:
+        raise HTTPException(status_code = 400 , detail = "Invalid field selection. Choose asc or desc.")
+   
+    sort_order = True if order == "desc" else False
+
+    sorted_data = sorted(data.values(), key = lambda x:x.get( sort_by, 0 ) , reverse = sort_order)
+    
+    return sorted_data
